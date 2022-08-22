e2f74cb76c7a16d7ea04c7cbf7c7457bc92130f9
Thebas57 <theohelf@gmail.com>
2022-08-22 13:45:23 +0200

    fil d'actualité

---
+++ client/src/actions/post.actions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export const GET_POSTS = "GET_POSTS";
+export const LIKE_POST = "LIKE_POST";
+export const UNLIKE_POST = "UNLIKE_POST";
+
+export const getPosts = (count) => {
+  return (dispatch) => {
+    return axios
+      .get(`${process.env.REACT_APP_API_URL}api/post`)
+      .then((res) => {
+        const array = res.data.slice(0, count);
+        dispatch({ type: GET_POSTS, payload: array });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const likePost = (postId, userId) => {
+  return (dispatch) => {
+    return axios
+      .patch(`${process.env.REACT_APP_API_URL}api/post/like-post/` + postId, {
+        id: userId,
+      })
+      .then((res) => {
+        dispatch({ type: LIKE_POST, payload: { postId, userId } });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const unlikePost = (postId, userId) => {
+  return (dispatch) => {
+    return axios
+      .patch(`${process.env.REACT_APP_API_URL}api/post/unlike-post/` + postId, {
+        id: userId,
+      })
+      .then((res) => {
+        dispatch({ type: UNLIKE_POST, payload: { postId, userId } });
+      })
+      .catch((err) => console.log(err));
+  };
+};
--- client/src/components/Profil/UpdateProfil.js
+++ client/src/components/Profil/UpdateProfil.js
@@ -76,12 +76,16 @@ const UpdateProfil = () => {
                         <img src={user.picture} alt="user-pic" />
                         <h4>{user.pseudo}</h4>
                         <div className="follow-handler">
-                          <FollowHandler idToFollow={user._id} />
+                          <FollowHandler
+                            idToFollow={user._id}
+                            type={"suggestion"}
+                          />
                         </div>
                       </li>
                     );
                   }
                 }
+                return null;
               })}
             </ul>
           </div>
@@ -103,12 +107,16 @@ const UpdateProfil = () => {
                         <img src={user.picture} alt="user-pic" />
                         <h4>{user.pseudo}</h4>
                         <div className="follow-handler">
-                          <FollowHandler idToFollow={user._id} />
+                          <FollowHandler
+                            idToFollow={user._id}
+                            type={"suggestion"}
+                          />
                         </div>
                       </li>
                     );
                   }
                 }
+                return null;
               })}
             </ul>
           </div>
