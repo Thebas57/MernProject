ca8b26d630ae0e062280717891cb04edbbc0329b
Thebas57 <theohelf@gmail.com>
2022-08-31 11:39:52 +0200

    post forumulaire et video yt

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import LeftNav from '../components/LeftNav';
-import Thread from '../components/Thread';
+import React from "react";
+import { useContext } from "react";
+import { UidContext } from "../components/AppContext";
+import LeftNav from "../components/LeftNav";
+import NewPostForm from "../components/Post/NewPostForm";
+import Thread from "../components/Thread";
+import Log from "../components/Log";
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <LeftNav />
-            <div className="main">
-                <Thread />
-            </div>
+  const uid = useContext(UidContext);
+
+  return (
+    <div className="home">
+      <LeftNav />
+      <div className="main">
+        <div className="home-header">
+            {uid ? <NewPostForm /> : <Log signin={true} signup={false} />}
         </div>
-    );
+        <Thread />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
